407175c4c3b290be0174d45e81c5cd8242c749df
Star2578 <nuttykingger@gmail.com>
2024-03-22 20:31:16 +0700

    responsive banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -34,12 +34,17 @@ export default function Banner(){
                     :""
             }
 
-            <div className="absolute top-16 left-1/2 -translate-x-1/2 pt-7 pb-4 pl-2 pr-2 w-[90%] text-center rounded-sm bg-white/40 backdrop-blur-md shadow-xl z-50">
-                <h1 className="text-5xl mb-5">Hotel Booking</h1>
-                <p className="text-2xl font-[300]">Fast and Easy - For you to spend your time enjoying your vacation</p>
+            <div className="absolute top-16 left-1/2 -translate-x-1/2 pt-6 pb-4 pl-2 pr-2 w-[90%] text-center rounded-sm bg-white/40 backdrop-blur-md shadow-xl z-50">
+                <h1 className="text-3xl mb-3 sm:text-4xl md:text-5xl md:mb-4 2xl:text-7xl">Hotel Booking</h1>
+                <p className="text-sm font-[300] sm:text-lg md:text-xl 2xl:text-3xl">Fast and Easy - For you to spend your time enjoying your vacation</p>
             </div>
 
-            <button onClick={(e)=>{e.stopPropagation(); router.push("/hotel")}} className="absolute font-[400] text-2xl right-12 bottom-10 w-fit h-fit p-3 rounded-lg bg-white/30 backdrop-blur-md shadow-xl z-40 hover:text-slate-100 hover:ring-2 hover:ring-slate-100 transition-all">
+            <button onClick={(e)=>{e.stopPropagation(); router.push("/hotel")}} className="
+            absolute font-[100] text-md right-7 bottom-6 w-fit h-fit p-3 rounded-lg bg-white/30 backdrop-blur-md shadow-xl z-40 hover:text-slate-100 hover:ring-2 hover:ring-slate-100 transition-all
+            md:text-xl md:right-10 md:bottom-8
+            lg:text-2xl lg:right-12 lg:bottom-10
+            xl:text-3xl xl:right-12 xl:bottom-10 xl:p-5 xl:rounded-xl
+            2xl:text-5xl 2xl:right-15 2xl:bottom-12 2xl:p-7 2xl:rounded-2xl">
                 Select Hotel
             </button>
         </div>
