912d9f5de485575b6a9de6a2d965999e7dd0075a
Kuntinun Onvinijvorakul <sundaypapa25@gmail.com>
2024-03-25 23:39:30 +0700

    Fixing BookingList component UI

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -9,6 +9,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import { CircularProgress } from "@mui/material"
 
 import { useState } from "react";
+import Link from "next/link"
+import dayjs from "dayjs"
 
 export default function BookingList({ myBookingData }: { myBookingData: populatedBookingItem[] }) {
     const { data: session, status } = useSession();
@@ -41,12 +43,18 @@ export default function BookingList({ myBookingData }: { myBookingData: populate
             <div className="text-slate-100 text-xl mt-5 px-5 md:text-2xl">You have {bookings.length} bookings</div>
             {bookings.length > 0 ? (
                 bookings.map((bookingItem: populatedBookingItem) => (
-                    <div className="bg-slate-200 rounded px-5 mx-5 py-2 my-5" key={bookingItem._id}>
-                        <div className="text-xl">{bookingItem.bookDate}</div>
+                    <div className="bg-slate-200 max-w-[700px] rounded px-5 mx-5 py-2 my-5" key={bookingItem._id}>
+                        <div className="text-xl">Booking Date : {dayjs(new Date(bookingItem.bookDate)).format("DD/MM/YYYY")}</div>
+                        
                         <div className="text-xl">Booking id: {bookingItem._id}</div>
-                        <div className="text-xl">Hotel: {bookingItem.hotel.name}</div>
+                        
+                        <div className="text-xl">
+                            Hotel: 
+                            <Link href={`/hotel/${bookingItem._id}`}>{bookingItem.hotel.name}</Link>
+                        </div>
+                    
                         <button
-                            className='max-w-[500px] ml-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[100%] transition-all'
+                            className='max-w-[400px] ml-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-[100%] transition-all'
                             onClick={() => removeBooking(bookingItem._id)}>
                             Remove Booking
                         </button>
@@ -60,13 +68,13 @@ export default function BookingList({ myBookingData }: { myBookingData: populate
             {
           <div className='absolute top-0 left-0 z-[9999] hidden items-center w-full h-full bg-black/30' ref={popupBox}>
             <div className="relative flex flex-col gap-y-3 mx-auto p-2 justify-center text-center
-                            rounded-lg text-green-500 text-2xl bg-slate-50 
+                            rounded-lg text-orange-600 text-2xl bg-slate-50 
                             border-2 border-slate-400 w-[450px] h-[250px]">
               <button className='absolute top-3 right-3'
                 onClick={()=>popupBox.current!.style.display = "none"}>
                 <CloseIcon fontSize="large" className=' text-slate-400 hover:text-slate-600'/>
               </button>
-              <CheckCircleOutlineIcon fontSize="large" className='self-center'/>
+              <CheckCircleOutlineIcon fontSize="large" className='self-center' color="warning" />
               <div>YOUR BOOKING HAS BEEN DELETED !</div>
             </div>
           
